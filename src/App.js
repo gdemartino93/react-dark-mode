f1ae6ac420f94ed58a1f55893a903e9fbf6fdfa6
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-29 23:51:22 +0100

    mapping data array to create single article

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { useState, useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import {BsFillMoonFill, BsFillSunFill} from "react-icons/bs";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [isDark , setIsDark] = useState(false);
   return (
-    <section className="section-center">
+    <section>
       <div className="container">
-        <h2>DARK MODE APP</h2>
+        <h2 className="text-center">DARK MODE APP</h2>
+        <button className="btn btn-outline-primary d-flex" onClick={()=>{setIsDark(!isDark)}}> {isDark ? <BsFillSunFill /> : <BsFillMoonFill/> } </button>
       </div>
+        <section className="article-section">
+          {data.map(el => {
+            return <article key={el.id} className="articolo col-3">
+                      <h3>{el.title}</h3>
+                  </article>
+          })}
+        </section>
     </section>
   );
 }
