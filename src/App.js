06222442cefdcac4dd45c38f74473f3ae6eae7e3
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-29 23:24:11 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+
+function App() {
+  return (
+    <section className="section-center">
+      <div className="container">
+        <h2>DARK MODE APP</h2>
+      </div>
+    </section>
+  );
+}
+
+export default App;
