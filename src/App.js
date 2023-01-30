92012dbdd7ba47fa177e7773314d179f624b31e0
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-01-30 01:13:47 +0100

    used hooks to change theme and btn

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Articolo from "./Articolo";
 
 function App() {
+  // usestate per gestire il cambio del tema 
+  const [theme , setTheme] = useState("light-mode");
+  // usestate per gestire il cambio del tasto
   const [isDark , setIsDark] = useState(false);
+
+  const changeTheme = () =>{
+    if (theme === "light-mode"){
+      setTheme("dark-mode")
+      setIsDark(true)
+    }
+    else{
+      setTheme("light-mode")
+      setIsDark(false)
+    }
+  }
+  useEffect(()=>{
+    // cambia la classe al tag html ogni volte che cambia il valore di theme
+    document.documentElement.className = theme;
+  },[theme])
+  
   return (
     <section>
       <div className="container">
-        <h2 className="text-center">DARK MODE APP</h2>
-        <button className="btn btn-outline-primary d-flex" onClick={()=>{setIsDark(!isDark)}}> {isDark ? <BsFillSunFill /> : <BsFillMoonFill/> } </button>
+        <h2 className="text-center">Mondo digitale</h2>
+        <button className="btn btn-outline-primary d-flex" onClick={changeTheme}> 
+          {isDark ? <BsFillSunFill /> : <BsFillMoonFill />}
+        </button>
       </div>
         <section className="article-section col-12">
           {data.map(el=>{
-           const {id,body,title} = el;
+           const {id} = el;
             return <Articolo key={id} {...el} />
           })}          
         </section>
--- src/Articolo.js
+++ src/Articolo.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Articolo = ({title,body}) => {
   return (
     
-    <article className="article col-lg-5 col-md-6 col-sm-6 col-12 shadow p-3 mb-5 bg-white rounded">
+    <article className="article col-lg-5 col-md-6 col-sm-6 col-12 shadow p-3 mb-5 rounded">
       <div className="article-title">
         <h4 className="fw-bold">{title}</h4>
         <div className="underline"></div>
